f7ea5ce81908fee7598c7230a3ed5bde7090f0cc
RAJ <107236972+raju1409@users.noreply.github.com>
2023-01-18 14:14:56 +0530

    Update index.js

--- get time/index.js
+++ get time/index.js
@@ -1,6 +1,6 @@
 
 let mylabel = document.getElementById("myLabel");
-
+setInterval(display, 1000);
 display();
 function display(){
   let date = new Date();
@@ -13,6 +13,14 @@ function display(){
     let amORpm = hours>=12 ? "PM":"AM";
 
     hours = hours%12 || 12;
+
+    hours = formatZero(hours);
+    minutes = formatZero(minutes);
+    seconds = formatZero(seconds);
     return `${hours} :${minutes} :${seconds} ${amORpm}`;
+  }
+  function formatZero(time){
+    time = time.toString();
+    return time.length<2 ? "0"+time : time;
   }
 }
