ebe310eb3d7bffc2b5a7b47ef43ecb45b558f68d
RAJ <rajurj.1249@gmail.com>
2023-01-20 11:40:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// window.addEventListener("keydown", event => console.log(event.key))
+
+const block24 = document.getElementById('mydiv');
+window.addEventListener('keydown', move);
+let x = 0;
+let y = 0;
+function move(event){
+    switch(event.key){
+        case "ArrowDown":
+            y+=15;
+            block24.style.top = y+'px';
+            break;
+        case "ArrowUp":
+            y-=15;
+            block24.style.top = y+'px';
+            break;
+        case "ArrowLeft":
+            x-=15;
+            block24.style.left = x+'px';
+            break;
+        case "ArrowRight":
+            x+=15;
+            block24.style.left = x+'px';
+            break;
+    }
+}
